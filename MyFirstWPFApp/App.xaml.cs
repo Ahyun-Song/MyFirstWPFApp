c1e1ae2e26cfbbb7437e19b3bd6eb029759b872b
Ahyun-Song <yoon9113@naver.com>
2025-01-13 11:26:13 +0900

    Add project files.

---
+++ MyFirstWPFApp/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using MyFirstWPFApp.Views;
+
+namespace MyFirstWPFApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow main = new();
+            main.ShowDialog();
+        }
+
+    }
+
+}
